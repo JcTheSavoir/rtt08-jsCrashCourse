45f38b55c98d25e3494b28d463e03fdbf5991c27
JcTheSavior <jacobconnell3@gmail.com>
2024-03-16 21:51:26 -0400

    Parts of object literalls

--- README.md
+++ README.md
@@ -66,3 +66,39 @@ console.log(Array.isArray(fruits));
 
 //Find the index of an item in an array
 console.log(fruits.indexOf('oranges'));
+
+/*--------------------OBJECT LITERALS---------------------------------------
+Can have strings, numbers, arrays, and even other objects; all contained within a single object*/
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+console.log(person);
+
+//to access a single value, use dot syntax
+console.log(person.firstName, person.lastName);
+
+//to access to a specific value inside the array in the object
+console.log(person.hobbies[1]);
+
+//to access a specific value inside an object that is inside an object
+console.log(person.address.city);
+
+//Destructuring of an object, to create variables of the items in the object
+const { firstName, lastName} = person;
+console.log(firstName);
+
+//Destructuring in order to create variables of items inside the object, inside the object
+const {address: {city}} = person;
+console.log(city);
+
+// add property to an exisiting object
+person.email = 'john@gmail.com';
+console.log(person);
--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-/* --------------------ARRAYS------------- 
-Variables that hold multiple values */
+/*--------------------OBJECT LITERALS---------------------------------------
+Can have strings, numbers, arrays, and even other objects; all contained within a single object*/
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+console.log(person);
 
-    //Constructor Array (No longer used as often)
+//to access a single value, use dot syntax
+console.log(person.firstName, person.lastName);
 
-const numbers = new Array(1,2,3,4,5);
-console.log(numbers)
+//to access to a specific value inside the array in the object
+console.log(person.hobbies[1]);
 
-//Normal way to construct arrays
-const fruits = ['apples', 'oranges', 'pears'];
-console.log(fruits);
+//to access a specific value inside an object that is inside an object
+console.log(person.address.city);
 
-//Access a single item from the "fruits" array
-console.log(fruits[1]); // brings back "oranges" because the index of arrays alwasy starts from 0, and goes up from there
+//Destructuring of an object, to create variables of the items in the object
+const { firstName, lastName} = person;
+console.log(firstName);
 
- //Add to an existing array
- fruits[3] = 'grapes';
- console.log(fruits);
-/*adds the new item based on the index called.  
- Since the fruits array didn't go up to index 3, it created a new index spot and put 'grapes' there.*/
+//Destructuring in order to create variables of items inside the object, inside the object
+const {address: {city}} = person;
+console.log(city);
 
-//Add to the end of an existing Array 
-fruits.push('mangos');
-console.log(fruits);
-
-// Add to the beginning of an exisitng Array
-fruits.unshift('strawberries');
-console.log(fruits);
-
-//Remove the last item of an exisitng array
-fruits.pop(fruits);
-console.log(fruits);
-
-//To check if something is an array
-console.log(Array.isArray(fruits));
-
-//Find the index of an item in an array
-console.log(fruits.indexOf('oranges'));
+// add property to an exisiting object
+person.email = 'john@gmail.com';
+console.log(person);
