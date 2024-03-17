fd2c0ac8309cffc73f562cf9aa22282c62f9d8fe
JcTheSavior <jacobconnell3@gmail.com>
2024-03-16 21:58:47 -0400

    Arrays, with objects inside

--- README.md
+++ README.md
@@ -102,3 +102,30 @@ console.log(city);
 // add property to an exisiting object
 person.email = 'john@gmail.com';
 console.log(person);
+
+
+//----------------------ARRAYS, MADE UP OF OBJECT LITERALS----------------------
+
+const todos = [
+    {
+        id: 1,
+        text: 'Take out Trash',
+        isCompleted: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false,
+    }
+];
+console.log(todos);
+
+//to access items in an object, inside an array
+console.log(todos[1].text);
+
+ 
--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-/*--------------------OBJECT LITERALS---------------------------------------
-Can have strings, numbers, arrays, and even other objects; all contained within a single object*/
-const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    hobbies: ['music', 'movies', 'sports'],
-    address: {
-        street: '50 Main st',
-        city: 'Boston',
-        state: 'MA'
+//----------------------ARRAYS, MADE UP OF OBJECT LITERALS----------------------
+
+const todos = [
+    {
+        id: 1,
+        text: 'Take out Trash',
+        isCompleted: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false,
     }
-}
-console.log(person);
+];
+console.log(todos);
 
-//to access a single value, use dot syntax
-console.log(person.firstName, person.lastName);
+//to access items in an object, inside an array
+console.log(todos[1].text);
 
-//to access to a specific value inside the array in the object
-console.log(person.hobbies[1]);
 
-//to access a specific value inside an object that is inside an object
-console.log(person.address.city);
 
-//Destructuring of an object, to create variables of the items in the object
-const { firstName, lastName} = person;
-console.log(firstName);
 
-//Destructuring in order to create variables of items inside the object, inside the object
-const {address: {city}} = person;
-console.log(city);
-
-// add property to an exisiting object
-person.email = 'john@gmail.com';
-console.log(person);
